db9df4e704f74b63ee106ba74d0319feb4fea349
Herman Tan <herman.tan@minerra.net>
2025-12-02 17:29:17 +0800

    Add common R file and data CSVs for blog post, fix source path

---
+++ my-blog/posts/2025-12-01-my-post/_common.R
@@ -0,0 +1,52 @@
+# Common settings for Transformative AI Replication Project
+# Following r4ds conventions
+
+# Set seed for reproducibility
+set.seed(1014)
+
+# Knitr options for code chunks
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  fig.retina = 2,
+  fig.width = 6,
+  fig.asp = 2 / 3,
+  fig.show = "hold"
+)
+
+# General options
+options(
+  dplyr.print_min = 6,
+  dplyr.print_max = 6,
+  pillar.max_footer_lines = 2,
+  pillar.min_chars = 15,
+  stringr.view_n = 6,
+  # Temporarily deactivate cli output for quarto
+  cli.num_colors = 0,
+  cli.hyperlink = FALSE,
+  pillar.bold = TRUE,
+  width = 77 # 80 - 3 for #> comment
+)
+
+# Set ggplot2 theme
+ggplot2::theme_set(ggplot2::theme_gray(12))
+
+# Load common packages
+library(tidyverse)
+library(lubridate)
+library(scales)
+library(knitr)
+library(kableExtra)
+library(here)
+library(stringr)
+
+# Project-specific settings
+PROJECT_START_DATE <- as.Date("2023-01-01")
+PROJECT_END_DATE <- as.Date("2025-11-30")
+N_PERMUTATIONS <- 5000
+EVENT_WINDOWS <- list(
+  short = list(length = 5,  first = -5,  last = 5),
+  long  = list(length = 15, first = -15, last = 15)
+)
+
